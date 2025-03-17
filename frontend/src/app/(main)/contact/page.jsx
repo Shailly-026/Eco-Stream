a0f64aba3bdcc6923c1652ca6990a64f3a9a21ab
mansi rawat <mansirawatm06409307@gmail.com>
2025-03-17 13:46:22 +0530

    contact us changes

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import { useFormik } from 'formik';
 import React from 'react'
-import * as Yup from 'yup';
+// import * as Yup from 'yup';
 
 const ContactSchema = Yup.object().shape({
   name:Yup.string()
